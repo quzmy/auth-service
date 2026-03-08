e45dbd9eb4012876913526900b709c9d0e1b25ad
quzmy <frizee2287@gmail.com>
2026-03-08 03:33:31 +0300

    database added

--- cmd/main.go
+++ cmd/main.go
@@ -2,5 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok")
+	})
+
+	http.ListenAndServe(":6767", r)
+}
